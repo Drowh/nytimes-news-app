86969c1817218a1a77a07d83bff29108a7f5f5b2
Andrey Drow <miupanka@gmail.com>
2025-09-22 17:34:54 +0300

    Enhance API

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -13,6 +13,20 @@ exports.handler = async (event, context) => {
     const response = await fetch(fullUrl);
     const data = await response.json();
 
+    // Filter and limit the response to reduce size
+    if (data.response && data.response.docs) {
+      // Take only first 50 articles and essential fields
+      data.response.docs = data.response.docs.slice(0, 50).map(article => ({
+        _id: article._id,
+        abstract: article.abstract,
+        web_url: article.web_url,
+        pub_date: article.pub_date,
+        source: article.source,
+        headline: article.headline,
+        multimedia: article.multimedia ? article.multimedia.slice(0, 3) : [] // Limit multimedia
+      }));
+    }
+
     return {
       statusCode: response.status,
       headers: {
